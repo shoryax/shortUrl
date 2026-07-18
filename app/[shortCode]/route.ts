4aa61345d597b02e89dfcbf080d2453eea2975db
shoryax <shoryavardhan7@gmail.com>
2026-07-18 16:15:04 +0530

    fixed the localhost error while redirecting

--- app/[shortCode]/route.ts
+++ app/[shortCode]/route.ts
@@ -1,4 +1,3 @@
-// app/[shortCode]/route.ts
 import { NextRequest, NextResponse } from "next/server";
 import { getOriginalUrl } from "@/services/url.service";
 
@@ -8,13 +7,13 @@ export async function GET(
 ) {
   const { shortCode } = await params;
 
-  const url = await getOriginalUrl(shortCode);
+  const url = await getOriginalUrl(shortCode, {
+    referrer: req.headers.get("referer") ?? undefined,
+    userAgent: req.headers.get("user-agent") ?? undefined,
+  });
 
   if (!url) {
-    return NextResponse.json(
-      { error: "Short URL not found" },
-      { status: 404 }
-    );
+    return NextResponse.json({ error: "Short URL not found" }, { status: 404 });
   }
 
   return NextResponse.redirect(url.originalUrl);
